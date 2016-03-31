2028c4a39ae731ea89c65b22b49fa550e2bb7cbe
Sirius1109 <gmh009@163.com>
2016-03-31 11:51:35 +0800

    3.31

--- 03_SmsSender/src/com/sirius/smssender/MainActivity.java
+++ 03_SmsSender/src/com/sirius/smssender/MainActivity.java
@@ -2,8 +2,11 @@ package com.sirius.smssender;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -13,20 +16,19 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void send(View v){
+		EditText et_phone = (EditText) findViewById(R.id.et_phone);
+		EditText et_content = (EditText) findViewById(R.id.et_content);
+		
+		String phone = et_phone.getText().toString();
+		String content = et_content.getText().toString();
+		
+		
+		//·¢ËÍ¶ÌÐÅ
+		SmsManager sm = SmsManager.getDefault();
+		
+		sm.sendTextMessage(phone, null, content, null, null);
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
+	
 }
